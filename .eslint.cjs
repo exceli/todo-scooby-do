31a90f17c754cc29a2697340b112d91b27b21f88
excel <danil_gorokhov@list.ru>
2024-08-12 20:42:40 +0500

    chore: setup eslint and prettier configuration

---
+++ .eslint.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        'prettier/prettier': 'error',
+        'react/react-in-jsx-scope': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+    },
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+};
